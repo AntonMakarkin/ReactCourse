0a65a95324c07c96f43ac74185af0596844b92a1
AntonMakarkin <amakarkin27@gmail.com>
2020-09-29 13:44:45 +0300

    Properties and states of components. Functions (theory)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+//import App from './components/app/';
+
+function WhoAmI({name, surname, link}) {
+    return (
+        <> 
+            <h1>My name is {name}, surname - {surname}</h1>
+            <a href={link}>My profile</a>
+        </>
+    )
+}
+
+const All = () => {
+    return (
+        <>
+           <WhoAmI name="John" surname="Smith" link="facebook.com"/>
+           <WhoAmI name="Ivan" surname="Ivanov" link="vk.com"/>
+           <WhoAmI name="Alex" surname="Shepard" link="facebook.com"/>
+        </>
+    )
+}
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<All/>, document.getElementById('root'));
 
