18b9eb633a029cde5c5314f54af20995bf4ee080
AntonMakarkin <amakarkin27@gmail.com>
2020-09-29 14:19:08 +0300

    Properties and states of components. Classes (theory)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './components/app/';
+class WhoAmI extends Component {
+    constructor(props) {
+        /*super(props);
+        this.state = {
+            years: 26,
+        }*/
+        //this.nextYear = this.nextYear.bind(this); //first way
+        /*this.nextYear = () => { //second way
+            this.setState(state => ({
+                years: ++state.years
+            }))
+        }*/
+    }
+    state = { //third way (experimental)
+        years: 26
+    }
 
-function WhoAmI({name, surname, link}) {
-    return (
-        <> 
-            <h1>My name is {name}, surname - {surname}</h1>
-            <a href={link}>My profile</a>
-        </>
-    )
+    nextYear = () => { //third way (experimental)
+        this.setState(state => ({
+            years: ++state.years
+        }))
+    }
+    /*nextYear() { //first way
+        this.setState(state => ({
+            years: ++state.years
+        }))
+    }*/
+    render() {
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return (
+            <> 
+                <button onClick={this.nextYear}>++</button>
+                <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+                <a href={link}>My profile</a>
+            </>
+        )
+    }
 }
+
 
 const All = () => {
     return (
