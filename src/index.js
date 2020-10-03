954dd9e2255bac96be78a03d35906bb9f04bc5be
AntonMakarkin <amakarkin27@gmail.com>
2020-10-03 23:43:08 +0300

    Properties and states of components. Practice

--- src/index.js
+++ src/index.js
@@ -1,58 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './components/app/';
-class WhoAmI extends Component {
-    constructor(props) {
-        /*super(props);
-        this.state = {
-            years: 26,
-        }*/
-        //this.nextYear = this.nextYear.bind(this); //first way
-        /*this.nextYear = () => { //second way
-            this.setState(state => ({
-                years: ++state.years
-            }))
-        }*/
-    }
-    state = { //third way (experimental)
-        years: 26
-    }
-
-    nextYear = () => { //third way (experimental)
-        this.setState(state => ({
-            years: ++state.years
-        }))
-    }
-    /*nextYear() { //first way
-        this.setState(state => ({
-            years: ++state.years
-        }))
-    }*/
-    render() {
-        const {name, surname, link} = this.props;
-        const {years} = this.state;
-        return (
-            <> 
-                <button onClick={this.nextYear}>++</button>
-                <h1>My name is {name}, surname - {surname}, years = {years}</h1>
-                <a href={link}>My profile</a>
-            </>
-        )
-    }
-}
+import App from './components/app/';
 
 
-const All = () => {
-    return (
-        <>
-           <WhoAmI name="John" surname="Smith" link="facebook.com"/>
-           <WhoAmI name="Ivan" surname="Ivanov" link="vk.com"/>
-           <WhoAmI name="Alex" surname="Shepard" link="facebook.com"/>
-        </>
-    )
-}
 
-
-ReactDOM.render(<All/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
